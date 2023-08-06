93b03960135e2724ca7569d543cdad0602fa5f66
Nastya-cr <137172870+Nastya-cr@users.noreply.github.com>
2023-08-06 21:11:14 +0300

    Add files via upload
    🥲

--- advertisements/app_lesson_4/models.py
+++ advertisements/app_lesson_4/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Advertisement(models.Model):
+    title= models.CharField('заголовок', max_length=128)
+    
+    description= models.TextField('описание')
+
+    price= models.DecimalField('цена', max_digits=10, decimal_places=2)
+   
+    
+    #auction= models.BooleanField('торг', help_text='отметьте,уместен ли торг')
+    created_at= models.DateTimeField(auto_now_add=True)
+
+    updated_at= models.DateTimeField(auto_now=True)
+
+    
+    class Meta:
+        db_table = "advertisement"
+    
+    
+    def __str__(self):
+        return f"Advertisement(id={self.pk}, title={self.title}, price={self.price})"
